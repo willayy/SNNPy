e2a80672fb7bc9c7372a9a6c5c65cf39a4182f48
William <williamnorland@gmail.com>
2023-10-31 20:59:39 +0100

    added some C function headers

---
+++ CiffPy/LinearRegressionModel.c
@@ -0,0 +1,26 @@
+struct LinearRegressionModel {
+    double b1;
+    double b0;
+    double r;
+};
+
+// Supply a list of x's and y's to fit a line to the regression model.
+//
+void fitModel( double xs [], double ys []) {
+
+}
+
+// Gets the r value (the linear relationship strenght)
+double getRVal() {
+
+}
+
+// Predicts a point Y given a point x
+double predictY(double x) {
+
+}
+
+// Predicts a mean for Y given a point x.
+double predictMean(double x) {
+
+}
