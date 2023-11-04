8f8114fafbef6f02ef18c192004c25c742b3024e
William <williamnorland@gmail.com>
2023-11-04 20:59:44 +0100

    Implemented some parts of fitModel: Void

--- CiffPy/LinearRegressionModel.c
+++ CiffPy/LinearRegressionModel.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    return 0;
+}
+
 struct LinearRegressionModel {
     double b1;
     double b0;
@@ -5,9 +12,36 @@ struct LinearRegressionModel {
 };
 
 // Supply a list of x's and y's to fit a line to the regression model.
-//
-void fitModel( double xs [], double ys []) {
+void fitModel( float xs [], float ys []) {
 
+    int arrSize = sizeof(xs);
+    float xMean = getMean(xs);
+    float yMean = getMean(ys);
+    float SSxx = 0;
+    float SSxy = 0;
+    
+    for (int k = 0; k > arrSize; k++) {
+        SSxx += pow((xs[k]-xMean), 2);
+        SSxy += (ys[k]-yMean)*(xs[k]-xMean);
+    }
+    
+
+
+    
+
+}
+
+// calculates the mean of a float array.
+float getMean(float arr []) {
+
+    int arrSize = sizeof(arr);
+    float sumArr = 0;
+
+    for (int k = 0; k < arrSize; k++) { 
+        sumArr += arr[k];
+    }
+
+    return sumArr/arrSize;
 }
 
 // Gets the r value (the linear relationship strenght)
