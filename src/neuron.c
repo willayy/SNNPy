6ca225ee7458a89d7afa43156e0fab77ad31d2e2
William <williamnorland@gmail.com>
2023-12-17 19:27:15 +0100

    Adding a neuron struct and initialization methods

---
+++ src/neuron.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Neuron
+{
+    double* weights; // The weights of the edges coming into this neuron
+    double output; // The output of this neuron
+};
+
+/**
+ * Initialize a starting vector of neurons
+ * @param num_inputs The number of parameters
+*/
+struct Neuron* makeParameterLayer(int num_inputs) {
+    struct Neuron* parameterLayer = malloc(sizeof(struct Neuron) * num_inputs);
+    for (int i = 0; i < num_inputs; i++)
+    {
+        parameterLayer[i] = initializeNeuron(0);
+    }
+}
+
+/**
+ * Initialize a neuron with weights set to 1
+ * @param num_inputs The number of inputs to this neuron
+ * @return The initialized neuron
+*/
+struct Neuron initializeNeuron(int num_inputs) {
+    struct Neuron neuron;
+    neuron.weights = malloc(sizeof(double) * num_inputs);
+    for (int i = 0; i < num_inputs; i++)
+    {
+        neuron.weights[i] = 1;
+    }
+    return neuron;
+}
+
+/**
+ * Free the memory allocated to a neuron
+ * @param neuron The neuron to free from memory
+*/
+void freeNeuron(struct Neuron *neuron) {
+    free(neuron->weights);
+}
