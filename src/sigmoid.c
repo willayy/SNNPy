07fa802c81a6877337d542794f7e35767fa6f806
William <williamnorland@gmail.com>
2023-12-17 19:27:25 +0100

    Adding a file for the sigmoid function

---
+++ src/sigmoid.c
@@ -0,0 +1,14 @@
+#include <math.h>
+
+
+static double e = 2.7182818284590452353602874;
+
+
+/**
+ * The sigmoid function (1 / (1 + e^-x)
+ * @param x The input value
+ * @return The sigmoid of the input value (lower bound 0, upper bound 1)
+*/
+double sigmoid(double x) {   
+    return 1 / (1 + pow(e,-x));
+}
