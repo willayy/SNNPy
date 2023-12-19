e94aa3997f5f512ebbb8e0c163acd60249bf0d1d
William <williamnorland@gmail.com>
2023-12-19 22:56:41 +0100

    Making neural network functions into its own .h file

---
+++ include/neuronFuncs.h
@@ -0,0 +1,8 @@
+#ifndef neuronFuncs_h
+    #define neuronFuncs_h
+
+    struct NeuronLayer createNeuronLayer(int edgesPerNeuron, int nrOfNeurons);
+
+    struct ParameterLayer createParameterLayer(int nrOfParameters);
+
+#endif
