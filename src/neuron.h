550468f6dc6263f6dece38c42f04effe5061e353
William <williamnorland@gmail.com>
2023-12-19 22:57:21 +0100

    removing combined neuron file

--- src/neuron.h
+++
@@ -1,5 +0,0 @@
-#ifndef neuron_h_
-#define neuron_h_
-struct NeuronLayer createNeuronLayer(int edgesPerNeuron, int nrOfNeurons);
-struct NeuralNetwork createNeuralNetwork(int nrOfParameters, int amountOfLayersInBytes);
-#endif
