d431531743e8213837cf88d7b81d2c60ea4c3cf5
William <williamnorland@gmail.com>
2023-12-19 23:00:50 +0100

    adding a simple debug test to main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,7 @@
-#include <neuron.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "../include/neuralNetworkFuncs.h"
+#include "../include/neuralNetworkStructs.h"
 
 int main() {
+    struct ParameterLayer parameterLayer = createParameterLayer(3);
     return 0;
-    
 }
