4c187ea2c8f8b112b65abbb1eba2ae0c633388cb
William <williamnorland@gmail.com>
2023-12-22 18:55:59 +0100

    Expanding the tiny demo program and adding functions to free memory

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "neuralNetworkStructs.h"
 
 int main() {
-    struct ParameterLayer parameterLayer = createParameterLayer(3);
+    struct ParameterLayer parameterLayer = createParameterLayer(10);
+    struct NeuronLayer neuronLayer = createNeuronLayer(3, 3);
     return 0;
 }
--- src/neuralNetworkFuncs.c
+++ src/neuralNetworkFuncs.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "neuralNetworkFuncs.h"
 #include "neuralNetworkStructs.h"
 
@@ -7,13 +8,13 @@
 struct ParameterLayer createParameterLayer(int nrOfParameters) {
     struct ParameterLayer parameterLayer;
     parameterLayer.size = nrOfParameters;
-    double parameters[nrOfParameters];
+
+    parameterLayer.parameters = (double *) malloc(nrOfParameters * sizeof(double));
 
     for (int i = 0; i < nrOfParameters; i++) {
-        parameters[i] = 0;
+        parameterLayer.parameters[i] = 69.00;
     }
-    
-    parameterLayer.parameters = parameters;
+
     return parameterLayer;
 }
 
@@ -24,17 +25,33 @@ struct ParameterLayer createParameterLayer(int nrOfParameters) {
 struct NeuronLayer createNeuronLayer(int edgesPerNeuron, int nrOfNeurons) {
     struct NeuronLayer neuronLayer;
     neuronLayer.size = nrOfNeurons;
-    double edges[edgesPerNeuron][nrOfNeurons];
-    double output[nrOfNeurons];
 
+    neuronLayer.edges = (double *) malloc(edgesPerNeuron * nrOfNeurons * sizeof(double));
+    neuronLayer.output = (double *) malloc(nrOfNeurons * sizeof(double));
+
+    for (int i = 0; i < edgesPerNeuron * nrOfNeurons; i++) {
+        neuronLayer.edges[i] = 69.00;
+    }
     for (int i = 0; i < nrOfNeurons; i++) {
-        output[i] = 0;
-        for (int j = 0; j < edgesPerNeuron; j++) {
-            edges[i][j] = 0;
-        }
+        neuronLayer.output[i] = 69.00;
     }
 
-    neuronLayer.edges = edges;
-    neuronLayer.output = output;
     return neuronLayer;
+}
+
+/**
+ * Frees a neuron layer from memory.
+ * @param neuronLayer: the neuron layer to free from memory. 
+*/
+void freeNeuronLayer(struct NeuronLayer neuronLayer) {
+    free(neuronLayer.edges);
+    free(neuronLayer.output);
+}
+
+/**
+ * Frees a parameter layer from memory.
+ * @param parameterLayer: the parameter layer to free from memory. 
+*/
+void freeParameterLayer(struct ParameterLayer parameterLayer) {
+    free(parameterLayer.parameters);
 }
