1d85f383f565ff42ee1f26a15098fd203c45ec5a
William <williamnorland@gmail.com>
2023-12-23 19:38:02 +0100

    Making a demo test

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
-#include "neuralNetworkFuncs.h"
+#include <stdio.h>
+#include "neuralNetworkInit.h"
 #include "neuralNetworkStructs.h"
+#include "vectorOperations.h"
 
 int main() {
-    struct ParameterLayer parameterLayer = createParameterLayer(10);
-    struct NeuronLayer neuronLayer = createNeuronLayer(3, 3);
+    struct NeuralNetwork n = createNeuralNetwork(2, 1, 2, 1);
+    struct ParameterLayer p = n.parameterLayer;
+    struct NeuronLayer l = *(n.intermediateLayers);
+    struct NeuronLayer o = n.outputLayer;
+    double * output = vectorMatrixMul(p.parameters,l.edges, 2, 2); 
+    printf("Output: %f, %f\n", output[0], output[1]);
     return 0;
 }
