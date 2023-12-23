b5ed3ff59c62816cac694edb3e0749ad392aaf15
William <williamnorland@gmail.com>
2023-12-23 18:54:48 +0100

    created .h file for neuralNetworkTraining

---
+++ include/neuralNetworkTraining.h
@@ -0,0 +1,8 @@
+#include "neuralNetworkStructs.h"
+
+#ifndef neuralNetworkTraining_h
+    #define neuralNetworkTraining_h
+
+    void trainOnData(struct NeuralNetwork network, double * input, double * desiredOutput);
+
+#endif
