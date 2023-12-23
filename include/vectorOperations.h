abbded77bc65c5383824a1e9f8665e7a31cdc864
William <williamnorland@gmail.com>
2023-12-23 18:54:36 +0100

    Created .h file for vector operations

---
+++ include/vectorOperations.h
@@ -0,0 +1,8 @@
+#ifndef vectorOperations_h
+#define vectorOperations_h
+
+double dotProduct(double *a, double *b, int vectorSize);
+
+double * matrixMul(double *a, int rowsA, int colsA, double *b, int rowsB, int colsB);
+
+#endif
