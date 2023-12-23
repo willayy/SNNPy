730fddbc944a246309440cb3f8c4be2fb1ff33c6
William <williamnorland@gmail.com>
2023-12-23 19:38:35 +0100

    implementing vector ops

---
+++ src/vectorOperations.c
@@ -0,0 +1,34 @@
+#include "vectorOperations.h"
+#include <stdlib.h>
+
+/**
+ * Calculates the dot product of two vectors of the same size.
+ * @param a The first vector.
+ * @param b The second vector.
+ * @param vectorSize The size of the vectors. */
+double dotProduct(double *a, double *b, int vectorSize) {
+    double sum = 0;
+
+    for (int i = 0; i < vectorSize; i++) {
+        sum += a[i] * b[i];
+    }
+
+    return sum; 
+}
+
+/**
+ * calculates the product of a vector multiplied by a matrix.
+ * Size of vector must be equal to the number of columns in the matrix.
+ * @param vector The vector.
+ * @param matrix The matrix.
+ * @param vectorSize The size of the vector.
+ * @param matrixCols The number of columns in the matrix. */
+double * vectorMatrixMul(double * vector, double * matrix, int vectorSize, int matrixCols) {
+    double * resultVector = (double*)malloc(vectorSize * sizeof(double));
+     
+    for (int i = 0; i < vectorSize; i++) {
+        resultVector[i] = dotProduct(vector, matrix + i*matrixCols, vectorSize);
+    }
+
+    return resultVector;
+}
