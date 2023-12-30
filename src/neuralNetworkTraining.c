3020fcaa513d2080f319b242c2d847b95a2e34d3
William <williamnorland@gmail.com>
2023-12-31 00:14:49 +0100

    adding a costfucntion to neural network training

--- src/neuralNetworkTraining.c
+++ src/neuralNetworkTraining.c
@@ -1,5 +1,26 @@
 #include "neuralNetworkTraining.h"
+#include "neuralNetworkStructs.h"
+#include <stdlib.h>
 
 void trainOnData(struct NeuralNetwork network, double * input, double * desiredOutput) {
     
+}
+
+/**
+ * Calculates the cost of the neural network on a given input and desired output. 
+ * Calculating how good bad the current biases and edges are for a certain input.
+ * @param output The output of the neural network.
+ * @param desiredOutput The desired output of the neural network.
+ * @param outputSize The size of the output array.
+ * @return The cost of the neural network on the given input and desired output.
+*/
+double costFunction(double * output, double * desiredOutput, int outputSize) {
+
+    double cost = 0;
+
+    for (int i = 0; i < outputSize; i++) {
+        cost += (output[i] - desiredOutput[i]) * (output[i] - desiredOutput[i]);
+    }
+
+    return cost;
 }
