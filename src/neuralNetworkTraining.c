6640a143273e0bee473567fdba6696faa2be50bc
William <williamnorland@gmail.com>
2024-01-02 23:19:56 +0100

    started on backpropogation algorithm

--- src/neuralNetworkTraining.c
+++ src/neuralNetworkTraining.c
@@ -1,10 +1,7 @@
 #include "neuralNetworkTraining.h"
+#include "neuralNetworkOperations.h"
 #include "neuralNetworkStructs.h"
 #include <stdlib.h>
-
-void trainOnData(struct NeuralNetwork network, double * input, double * desiredOutput) {
-    
-}
 
 /**
  * Calculates the cost of the neural network on a given input and desired output. 
@@ -12,15 +9,66 @@ void trainOnData(struct NeuralNetwork network, double * input, double * desiredO
  * @param output The output of the neural network.
  * @param desiredOutput The desired output of the neural network.
  * @param outputSize The size of the output array.
- * @return The cost of the neural network on the given input and desired output.
-*/
-double costFunction(double * output, double * desiredOutput, int outputSize) {
+ * @return The cost of the neural network on the given input and desired output. */
+double * costFunction(double * output, double * desiredOutput, int outputSize) {
 
-    double cost = 0;
+    double cost[outputSize];
 
     for (int i = 0; i < outputSize; i++) {
-        cost += (output[i] - desiredOutput[i]) * (output[i] - desiredOutput[i]);
+        cost[i] = (output[i] - desiredOutput[i]);
     }
 
     return cost;
 }
+
+/**
+ * Backpropagates the cost through the output layer.
+ * @param nn The neural network.
+ * @param cost The cost of the neural network on a given input and desired output.
+ * @param outputSize The size of the output array. */
+void bpOutputLayer(struct NeuralNetwork nn, double * cost, int outputSize) {
+    double * edges = nn.outputLayer.edges;
+    double * biases = nn.outputLayer.biases;
+    for (int i = 0; i < outputSize; i++) {
+        for (int j = 0; j < nn.neuronsPerLayer; j++) {
+            edges[j] = edges[j] + cost[i];
+            biases[j] = biases[j] + cost[i];
+        }
+        edges += nn.neuronsPerLayer;
+    }
+}
+
+/**
+ * Backpropagates the cost through the intermediate layers.
+ * @param nn The neural network.
+ * @param cost The cost of the neural network on a given input and desired output.
+ * @param outputSize The size of the output array. */
+void bpIntermediateLayers(struct NeuralNetwork nn, double * cost, int outputSize) {
+    for (int i = 0; i < nn.nrOfLayers; i++) {
+        
+    }
+}
+
+/**
+ * Backpropagates the cost through the neural network.
+ * @param nn The neural network.
+ * @param cost The cost of the neural network on a given input and desired output.
+ * @param outputSize The size of the output array. */
+void backPropagate(struct NeuralNetwork nn, double * cost, int outputSize) {
+    bpOutputLayer(nn, cost, outputSize);
+    bpIntermediateLayers(nn, cost, outputSize);
+}
+
+/**
+ * Trains the neural network on a given input and desired output.
+ * @param nn The neural network.
+ * @param input The input of the neural network.
+ * @param desiredOutput The desired output of the neural network. */
+void trainOnData(struct NeuralNetwork nn, double * input, double * desiredOutput) {
+    double * output = inputDataToNeuralNetwork(nn, input);
+    double * cost = costFunction(output, desiredOutput, nn.nrOfOutputs);
+    backPropagate(nn, cost, nn.nrOfOutputs);
+}
+
+
+
