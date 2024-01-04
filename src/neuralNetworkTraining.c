3ef38161613cef41d42b9a982db9536da8edc63d
William <williamnorland@gmail.com>
2024-01-04 23:00:12 +0100

    Wiping training so it can be remade

--- src/neuralNetworkTraining.c
+++ src/neuralNetworkTraining.c
@@ -22,41 +22,12 @@ double * costFunction(double * output, double * desiredOutput, int outputSize) {
 }
 
 /**
- * Backpropagates the cost through the output layer.
- * @param nn The neural network.
- * @param cost The cost of the neural network on a given input and desired output.
- * @param outputSize The size of the output array. */
-void bpOutputLayer(struct NeuralNetwork nn, double * cost, int outputSize) {
-    double * edges = nn.outputLayer.edges;
-    double * biases = nn.outputLayer.biases;
-    for (int i = 0; i < outputSize; i++) {
-        for (int j = 0; j < nn.neuronsPerLayer; j++) {
-            edges[j] = edges[j] + cost[i];
-            biases[j] = biases[j] + cost[i];
-        }
-        edges += nn.neuronsPerLayer;
-    }
-}
-
-/**
- * Backpropagates the cost through the intermediate layers.
- * @param nn The neural network.
- * @param cost The cost of the neural network on a given input and desired output.
- * @param outputSize The size of the output array. */
-void bpIntermediateLayers(struct NeuralNetwork nn, double * cost, int outputSize) {
-    for (int i = 0; i < nn.nrOfLayers; i++) {
-        
-    }
-}
-
-/**
  * Backpropagates the cost through the neural network.
  * @param nn The neural network.
  * @param cost The cost of the neural network on a given input and desired output.
  * @param outputSize The size of the output array. */
 void backPropagate(struct NeuralNetwork nn, double * cost, int outputSize) {
-    bpOutputLayer(nn, cost, outputSize);
-    bpIntermediateLayers(nn, cost, outputSize);
+    
 }
 
 /**
@@ -65,9 +36,7 @@ void backPropagate(struct NeuralNetwork nn, double * cost, int outputSize) {
  * @param input The input of the neural network.
  * @param desiredOutput The desired output of the neural network. */
 void trainOnData(struct NeuralNetwork nn, double * input, double * desiredOutput) {
-    double * output = inputDataToNeuralNetwork(nn, input);
-    double * cost = costFunction(output, desiredOutput, nn.nrOfOutputs);
-    backPropagate(nn, cost, nn.nrOfOutputs);
+    
 }
 
 
