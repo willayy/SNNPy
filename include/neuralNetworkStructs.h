468586f694a940ad5d816fdf5f1db56483d294eb
William <williamnorland@gmail.com>
2024-01-04 22:58:32 +0100

    Remaking neural network structure to make operations simpler

--- include/neuralNetworkStructs.h
+++ include/neuralNetworkStructs.h
@@ -2,44 +2,26 @@
     #define neuronStructs_h
 
     /**
-     * A layer of parameters.
-     * @param size: the number of parameters in the layer.
-     * @param parameters: the double values of the parameters in the layer. */
-    struct ParameterLayer {
-        int size;
-        double * parameters;
-    };
-
-    /**
-     * A hidden layer of neurons.
-     * @param size: the number of neurons in the layer.
-     * @param edges: the weights of the edges between the neurons and the previous layer.
-     * @param output: the output double values of the neurons in the layer. */
-    struct NeuronLayer {
-        int size;
-        double * edges;
-        double * biases;
-        double * output;
-    };
-
-    /**
      * A neural network
      * @param nrOfParameters: the number of parameters in the network.
      * @param nrOfLayers: the number of layers in the network.
      * @param neuronsPerLayer: the number of neurons per layer in the network.
      * @param nrOfOutputs: the number of outputs in the network.
-     * @param parameter: the parameter layer of the network.
-     * @param layers: Pointer to the first layer of the network. 
-     * @param output: the output layer of the network.
-     * */
+     * @param parameterVector: the vector of parameters in the network.
+     * @param weightMatrix: the matrix of weights in the network.
+     * @param biasVector: the vector of biases in the network.
+     * @param outputVector: the vector of outputs in the network.
+     * @param neuronVector: the vector of neurons in the network. */
     struct NeuralNetwork {
         int nrOfParameters;
         int nrOfLayers;
         int neuronsPerLayer;
         int nrOfOutputs;
-        struct ParameterLayer parameterLayer;
-        struct NeuronLayer * intermediateLayers;
-        struct NeuronLayer outputLayer;
+        double * parameterVector;
+        double * weightMatrix;
+        double * biasVector;
+        double * outputVector;
+        double * neuronVector;
     };
 
 #endif
