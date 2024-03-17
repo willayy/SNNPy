bb3e884520f149c4120564fb74d56d8355764e83
William Norland <williamnorland@gmail.com>
2024-03-17 19:19:10 +0100

    Remove unused function headers

--- src/include/neuralNetworkUtility.h
+++ src/include/neuralNetworkUtility.h
@@ -5,17 +5,5 @@
 
         int * findConnectedNeuronIndexes(NeuralNetwork * nn, int neuron);
 
-        double * findConnectedNeuronActivations(NeuralNetwork * nn, int neuron);
-
-        double * findConnectedNeuronValues(NeuralNetwork * nn, int neuron);
-
-        double * findOutputWeights(NeuralNetwork * nn, int neuron);
-
-        double * findConnectedNeuronBiases(NeuralNetwork * nn, int neuron);
-
-        int isNeuronLastInLayer(NeuralNetwork * nn, int neuron);
-
-        int isNeuronLastInHiddenlayer(NeuralNetwork * nn, int neuron);
-
         int numberOfConnectedNeurons(NeuralNetwork * nn, int neuron);
 #endif
